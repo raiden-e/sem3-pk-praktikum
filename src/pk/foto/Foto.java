c214e6631db1e75af0c4e51fbcd5a6b672c336a3
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-10-15 11:41:44 +0200

    use association for FotoMetadaten

--- src/pk/foto/Foto.java
+++ src/pk/foto/Foto.java
@@ -3,11 +3,12 @@ package pk.foto;
 public class Foto {
     private String name;
     private String dateiName;
+    private FotoMetadaten metadaten;
 
     public void drucke() {
         System.out.println("Fotoname: " + name);
         System.out.println("Dateiname: " + dateiName);
-        System.out.println(FotoMetadaten.toString());
+        System.out.println(metadaten);
     };
 
     public String toString() {
