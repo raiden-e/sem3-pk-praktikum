de1921401895a73a30b0492212c4f3f879d60b73
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-10-15 11:21:16 +0200

    Indent using spaces

--- src/pk/FirstProgramm.java
+++ src/pk/FirstProgramm.java
@@ -1,25 +1,25 @@
 package pk;
 
 public class FirstProgramm {
-	public static void main(String[] args) throws Exception {
-		// This is a comment
-		maleTreppe(8, 3);
-		maleTreppe(6, 2);
-	}
+    public static void main(String[] args) throws Exception {
+        // This is a comment
+        maleTreppe(8, 3);
+        maleTreppe(6, 2);
+    }
 
-	// Kommentar
-	public static void maleTreppe(int hoehe, int stufentiefe) {
-		String baustein = "";
-		for (int i = 0; i < stufentiefe; i++)
-			baustein += "*";
+    // Kommentar
+    public static void maleTreppe(int hoehe, int stufentiefe) {
+        String baustein = "";
+        for (int i = 0; i < stufentiefe; i++)
+            baustein += "*";
 
-		String spacing = "";
-		for (int i = 0; i < hoehe * stufentiefe; i++)
-			spacing += " ";
-		String stufe = "";
-		for (int i = 0; i < hoehe; i++) {
-			stufe += baustein;
-			System.out.println(spacing.substring(0, ((hoehe - i) * stufentiefe)) + stufe);
-		}
-	}
+        String spacing = "";
+        for (int i = 0; i < hoehe * stufentiefe; i++)
+            spacing += " ";
+        String stufe = "";
+        for (int i = 0; i < hoehe; i++) {
+            stufe += baustein;
+            System.out.println(spacing.substring(0, ((hoehe - i) * stufentiefe)) + stufe);
+        }
+    }
 }
