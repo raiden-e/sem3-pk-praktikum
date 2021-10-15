18a75b48dff1da210533c5d091b4f347dd10f4b7
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-10-15 13:37:47 +0200

    Complete Tester

--- src/pk/foto/Tester.java
+++ src/pk/foto/Tester.java
@@ -3,5 +3,9 @@ package pk.foto;
 public class Tester {
     public static void main(String[] args) {
         FotoVerwaltung fotoverwaltung1 = new FotoVerwaltung();
+        fotoverwaltung1.addAlbum(new Album(new Foto("Foto1", "Foto1.png"), new Foto("Foto2", "Foto2.png")));
+        fotoverwaltung1.addAlbum(new Album(new Foto("Foto1", "Foto3.jpg"), new Foto("Foto2", "Foto4.jpg")));
+
+        fotoverwaltung1.druckeAlleAlben();
     }
 }
