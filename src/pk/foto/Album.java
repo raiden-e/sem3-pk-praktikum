6ed45087a5c3757385e0c3dc8224b293b3adad45
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-10-15 12:00:50 +0200

    indent fotos

--- src/pk/foto/Album.java
+++ src/pk/foto/Album.java
@@ -1,29 +1,27 @@
 package pk.foto;
 
-
 public class Album {
+    private String name;
+    private String besitzer;
 
-	private String name;
-	private String besitzer;
-	
-	Foto[] fotos = new Foto[2];
-	
-	public void drucke() {
-		System.out.println(String.format("Name: %s\nBesitzer: %s", name, besitzer));
-		int counter = 1;
-		for (Foto foto : fotos) 
-			System.out.println(String.format("=== Foto %s ===\n%s", counter++, foto));
-	}
-	
-	public Foto[] getFotos() {
-		
-	}
-	
-	public void addFoto(Foto foto) {
-		
-	}
-	
-	public String toString() {
-		
-	}
+    Foto[] fotos = new Foto[2];
+
+    public void drucke() {
+        System.out.println(String.format("Name: %s\nBesitzer: %s", name, besitzer));
+        int counter = 1;
+        for (Foto foto : fotos)
+            System.out.println(String.format("=== Foto %s ===\n%-4s", counter++, foto));
+    }
+
+    public Foto[] getFotos() {
+
+    }
+
+    public void addFoto(Foto foto) {
+
+    }
+
+    public String toString() {
+
+    }
 }
--- src/pk/foto/FotoVerwaltung.java
+++ src/pk/foto/FotoVerwaltung.java
@@ -1,27 +1,26 @@
 package pk.foto;
 
-
 public class FotoVerwaltung {
-	
-	Album[] alben = new Album[2];
 
-	public void druckeAlleAlben() {
-		
-	}
-	
-	public int gibAnzahlAlben() {
-		
-	}
-	
-	public Album[] gibAlleAlben() {
-		
-	}
-	
-	public void addAlbum(Album album) {
-		
-	}
-	
-	public Album findeAlbumMitName(String name) {
-		
-	}
+    Album[] alben = new Album[2];
+
+    public void druckeAlleAlben() {
+
+    }
+
+    public int gibAnzahlAlben() {
+
+    }
+
+    public Album[] gibAlleAlben() {
+
+    }
+
+    public void addAlbum(Album album) {
+
+    }
+
+    public Album findeAlbumMitName(String name) {
+
+    }
 }
