c2eeb18ca4d319428558a1f12c4d691d15bffedf
Henry Küfner <Henry Kfner@DESKTOP-S485DUA.fritz.box>
2021-10-15 11:23:31 +0200

    Album und FotoVerwaltung erweitert

--- src/pk/foto/Album.java
+++ src/pk/foto/Album.java
@@ -2,4 +2,22 @@ package pk.foto;
 
 public class Album {
 
+	private String name;
+	private String besitzer;
+	
+	public void drucke() {
+		
+	}
+	
+	public Foto[] getFotos() {
+		
+	}
+	
+	public void addFoto(Foto foto) {
+		
+	}
+	
+	public String toString() {
+		
+	}
 }
--- src/pk/foto/FotoVerwaltung.java
+++ src/pk/foto/FotoVerwaltung.java
@@ -1,9 +1,28 @@
 package pk.foto;
 
+import java.util.LinkedList;
+
 public class FotoVerwaltung {
+	
+	LinkedList<Album> alben = new LinkedList();
 
 	public void druckeAlleAlben() {
 		
 	}
 	
+	public int gibAnzahlAlben() {
+		
+	}
+	
+	public Album[] gibAlleAlben() {
+		
+	}
+	
+	public void addAlbum(Album album) {
+		
+	}
+	
+	public Album findeAlbumMitName(String name) {
+		
+	}
 }
