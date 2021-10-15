56d71a8086c3fb6902f66b925e7a2b14800d8222
Henry Küfner <Henry Kfner@DESKTOP-S485DUA.fritz.box>
2021-10-15 13:33:46 +0200

    Foto: Konstruktor erstellt

--- src/pk/foto/Foto.java
+++ src/pk/foto/Foto.java
@@ -1,12 +1,15 @@
 package pk.foto;
+import java.time.LocalDateTime;
 
 public class Foto {
     private String name;
     private String dateiName;
     private FotoMetadaten metadaten;
     
-    public Foto(String pName, String pDateiName) {
-    	
+    public Foto(String name, String dateiName, int breite, int hoehe, String kameraMarke, String kameraModell, LocalDateTime erstellungszeitpunkt) {
+    	this.name = name;
+    	this.dateiName = dateiName;
+    	metadaten = new FotoMetadaten(breite, hoehe, kameraMarke, kameraModell, erstellungszeitpunkt);
     }
 
     public void drucke() {
